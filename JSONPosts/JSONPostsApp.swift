3e63e2f03dfdb1d248d319dfab3a5e09798399d9
Mickaël MAS <appiwedia@gmail.com>
2021-12-07 10:38:37 +0100

    Initial Commit

---
+++ JSONPosts/JSONPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONPostsApp.swift
+//  JSONPosts
+//
+//  Created by Mickael Mas on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct JSONPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
